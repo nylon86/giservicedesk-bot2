47a48d483492a752162e9e5870e20a1881be6b84
lvallone <luca.vallone@enginium.eu>
2021-01-21 15:26:07 +0100

    update gitignore

--- dist/dialogs/qnamakerBaseDialog.d.ts
+++
@@ -1,8 +0,0 @@
-import { QnAMakerDialog } from 'botbuilder-ai';
-export declare class QnAMakerBaseDialog extends QnAMakerDialog {
-    /**
-     * Core logic of QnA Maker dialog.
-     * @param {QnAMaker} qnaService A QnAMaker service object.
-     */
-    constructor(knowledgebaseId: any, authkey: any, host: any);
-}
--- dist/logger/MyLogger.d.ts
+++
@@ -1,8 +0,0 @@
-import { Activity, TranscriptLogger } from 'botbuilder';
-export declare class MyLogger implements TranscriptLogger {
-    /**
-     * Log an activity to the transcript.
-     * @param activity Activity being logged.
-     */
-    logActivity(activity: Activity): void | Promise<void>;
-}
