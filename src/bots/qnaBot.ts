425d8580b70e478089563878653ce2e3f13b06a2
lvallone <luca.vallone@enginium.eu>
2021-02-01 16:26:40 +0100

    aggiunto esempio cards e migliorato sviluppo typescript

--- src/bots/qnaBot.ts
+++ src/bots/qnaBot.ts
@@ -1,9 +1,8 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { ActivityHandler, TurnContext } from 'botbuilder';
-import { QnAMaker } from 'botbuilder-ai';
-import { Logger } from '../server';
+import {ActivityHandler, ActivityTypes, CardFactory, TurnContext} from 'botbuilder';
+import {QnAMaker} from 'botbuilder-ai';
+import * as ACData from 'adaptivecards-templating';
+import * as AdaptiveCards from 'adaptivecards';
+// import {Logger} from '../server';
 /**
  * A simple bot that responds to utterances with answers from QnA Maker.
  * If an answer is not found for an utterance, the bot responds with help.
@@ -23,90 +22,121 @@ export class QnABot extends ActivityHandler {
     public qnaMaker: QnAMaker;
     /**
      *
-     * @param {ConversationState} conversationState
-     * @param {UserState} userState
-     * @param {Dialog} dialog
+     * @param {any} conversationState
+     * @param {any} userState
+     * @param {any} dialog
+     * @param {any} conversationReferences
      */
     constructor(conversationState: any, userState: any, dialog: any, conversationReferences: any) {
-        super();
-        if (!conversationState) throw new Error('[QnABot]: Missing parameter. conversationState is required');
-        if (!userState) throw new Error('[QnABot]: Missing parameter. userState is required');
-        if (!dialog) throw new Error('[QnABot]: Missing parameter. dialog is required');
+      super();
+      if (!conversationState) throw new Error('[QnABot]: Missing parameter. conversationState is required');
+      if (!userState) throw new Error('[QnABot]: Missing parameter. userState is required');
+      if (!dialog) throw new Error('[QnABot]: Missing parameter. dialog is required');
 
-        this.conversationState = conversationState;
-        this.userState = userState;
-        this.dialog = dialog;
-        this.dialogState = conversationState.createProperty('DialogState');
-        // this.conversationReferences1 = conversationReferences;
-        this.conversationDataAccessor = conversationState.createProperty(CONVERSATION_DATA_PROPERTY);
-        this.userProfileAccessor = userState.createProperty(USER_PROFILE_PROPERTY);
+      this.conversationState = conversationState;
+      this.userState = userState;
+      this.dialog = dialog;
+      this.dialogState = conversationState.createProperty('DialogState');
+      // this.conversationReferences1 = conversationReferences;
+      this.conversationDataAccessor = conversationState.createProperty(CONVERSATION_DATA_PROPERTY);
+      this.userProfileAccessor = userState.createProperty(USER_PROFILE_PROPERTY);
 
-        // const dispatchRecognizer = new LuisRecognizer({
-        //     applicationId: process.env.LuisAppId,
-        //     endpointKey: process.env.LuisAPIKey,
-        //     endpoint: `https://${process.env.LuisAPIHostName}.api.cognitive.microsoft.com`
-        // }, {
-        //     includeAllIntents: true,
-        //     includeInstanceData: true
-        // }, true);
+      // const dispatchRecognizer = new LuisRecognizer({
+      //     applicationId: process.env.LuisAppId,
+      //     endpointKey: process.env.LuisAPIKey,
+      //     endpoint: `https://${process.env.LuisAPIHostName}.api.cognitive.microsoft.com`
+      // }, {
+      //     includeAllIntents: true,
+      //     includeInstanceData: true
+      // }, true);
 
-        const qnaMaker = new QnAMaker({
-            endpointKey: process.env.QnAEndpointKey,
-            host: process.env.QnAEndpointHostName,
-            knowledgeBaseId: process.env.QnAKnowledgebaseId
-        });
+      const qnaMaker = new QnAMaker({
+        endpointKey: process.env.QnAEndpointKey,
+        host: process.env.QnAEndpointHostName,
+        knowledgeBaseId: process.env.QnAKnowledgebaseId,
+      });
 
-        // this.dispatchRecognizer = dispatchRecognizer;
-        this.qnaMaker = qnaMaker;
+      // this.dispatchRecognizer = dispatchRecognizer;
+      this.qnaMaker = qnaMaker;
 
-        this.onMessage(async (context, next) => {
-            // Logger.log({ level: 'info', message: context.activity.text });
-            console.log('Running dialog with Message Activity.');
-            const userProfile = await this.userProfileAccessor.get(context, {});
-            const conversationData = await this.conversationDataAccessor.get(
-                context, { promptedForUserName: false }
-            );
-            // Logger.log({ level: 'info', message: conversationData });
-            // Logger.log({ level: 'info', message: userProfile });
-            addConversationReference(context.activity);
-            // Run the Dialog with the new message Activity.
-            await this.dialog.run(context, this.dialogState);
-            Logger.log({ level: 'info', message: context.activity.text });
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-        // If a new user is added to the conversation, send them a greeting message
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (const member of membersAdded) {
-                if (member.id !== context.activity.recipient.id) {
-                    await context.sendActivity('Benvenuto nel servizio di assistenza del portale Zucchetti');
-                }
-            }
-
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-        this.onTurn(async (turnContext, next) => {
-            Logger.log({ level: 'info', message: 'INIZIO MESSAGGIO' });
-            Logger.log({ level: 'info', message: turnContext.activity.text });
-            Logger.log({ level: 'info', message: 'FINE MESSAGGIO' });
-            await next();
-        });
-
-        this.onDialog(async (context, next) => {
-            // Save any state changes. The load happened during the execution of the Dialog.
-            await this.conversationState.saveChanges(context, false);
-            await this.userState.saveChanges(context, false);
-
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-
-        function addConversationReference(activity: any): void {
-            const conversationReference = TurnContext.getConversationReference(activity);
-            conversationReferences[conversationReference.conversation.id] = conversationReference;
+      this.onMessage(async (context, next) => {
+        // Logger.log({ level: 'info', message: context.activity.text });
+        console.log('Running dialog with Message Activity.');
+        // const userProfile = await this.userProfileAccessor.get(context, {});
+        // const conversationData = await this.conversationDataAccessor.get(
+        // context, {promptedForUserName: false},
+        // );
+        // Logger.log({ level: 'info', message: conversationData });
+        // Logger.log({ level: 'info', message: userProfile });
+        addConversationReference(context.activity);
+        // Run the Dialog with the new message Activity.
+        await this.dialog.run(context, this.dialogState);
+        // const a: Activity = context.activity;
+        // console.log('new log prova: ' + JSON.stringify(a));
+        // By calling next() you ensure that the next BotHandler is run.
+        await next();
+      });
+      // If a new user is added to the conversation, send them a greeting message
+      this.onMembersAdded(async (context, next) => {
+        const membersAdded = context.activity.membersAdded;
+        for (const member of membersAdded) {
+          if (member.id !== context.activity.recipient.id) {
+            await context.sendActivity('Benvenuto nel servizio di assistenza del portale Zucchetti');
+          }
         }
+
+        // By calling next() you ensure that the next BotHandler is run.
+        await next();
+      });
+
+      this.onTurn(async (turnContext, next) => {
+        if (turnContext.activity.type === ActivityTypes.Message) {
+          const results = await this.qnaMaker.getAnswers(turnContext);
+          // console.log('eccola!! -> '+ JSON.stringify(results));
+          console.log('eccola!!   -> '+ results[0].answer);
+          const templatePayload = {
+            'type': 'AdaptiveCard',
+            'version': '1.0',
+            'body': [
+              {
+                'type': 'TextBlock',
+                'text': 'Hello ${name}!',
+              },
+            ],
+          };
+          const template = new ACData.Template(templatePayload);
+          // Expand the template with your `$root` data object.
+          // This binds it to the data and produces the final Adaptive Card payload
+          const cardPayload = template.expand({
+            $root: {
+              name: results[0].answer,
+            },
+          });
+
+          // OPTIONAL: Render the card (requires that the adaptivecards library be loaded)
+          const adaptiveCard = new AdaptiveCards.AdaptiveCard();
+          adaptiveCard.parse(cardPayload);
+          await turnContext.sendActivity({
+            text: 'Hero Card',
+            attachments: [CardFactory.adaptiveCard(adaptiveCard)],
+          });
+          return;
+        }
+        await next();
+      });
+
+      this.onDialog(async (context, next) => {
+        // Save any state changes. The load happened during the execution of the Dialog.
+        await this.conversationState.saveChanges(context, false);
+        await this.userState.saveChanges(context, false);
+        // By calling next() you ensure that the next BotHandler is run.
+        await next();
+      });
+
+      function addConversationReference(activity: any): void {
+        const conversationReference = TurnContext.getConversationReference(activity);
+        conversationReferences[conversationReference.conversation.id] = conversationReference;
+      }
     }
 }
 
--- src/dialogs/rootDialog.ts
+++ src/dialogs/rootDialog.ts
@@ -1,62 +1,61 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { StatePropertyAccessor, TurnContext } from 'botbuilder';
+import {StatePropertyAccessor, TurnContext} from 'botbuilder';
 import {
-    ComponentDialog,
-    DialogSet,
-    DialogState,
-    DialogTurnStatus,
-    WaterfallDialog
+  ComponentDialog,
+  DialogSet,
+  DialogState,
+  DialogTurnStatus,
+  WaterfallDialog,
 } from 'botbuilder-dialogs';
-
-import {
-    QnAMakerBaseDialog
-} from './qnamakerBaseDialog';
+import {QnAMakerBaseDialog} from './qnamakerBaseDialog';
 
 const INITIAL_DIALOG = 'initial-dialog';
 const ROOT_DIALOG = 'root-dialog';
 const QNAMAKER_BASE_DIALOG = 'qnamaker-base-dailog';
-
+/**
+ * Componente
+ */
 export class RootDialog extends ComponentDialog {
-    /**
+  /**
      * Root dialog for this bot.
-     * @param {QnAMaker} qnaService A QnAMaker service object.
+     * @param {string} knowledgebaseId A QnAMaker service object.
+     * @param {string} authkey A QnAMaker service object.
+     * @param {string} host A QnAMaker service object.
      */
-    constructor(knowledgebaseId: string, authkey: string, host: string) {
-        super(ROOT_DIALOG);
-        // Initial waterfall dialog.
-        this.addDialog(new WaterfallDialog(INITIAL_DIALOG, [
-            this.startInitialDialog.bind(this)
-        ]));
-        this.addDialog(new QnAMakerBaseDialog(knowledgebaseId, authkey, host));
-        this.initialDialogId = INITIAL_DIALOG;
+  constructor(knowledgebaseId: string, authkey: string, host: string) {
+    super(ROOT_DIALOG);
+    // Initial waterfall dialog.
+    this.addDialog(new WaterfallDialog(INITIAL_DIALOG, [
+      this.startInitialDialog.bind(this),
+    ]));
+    this.addDialog(new QnAMakerBaseDialog(knowledgebaseId, authkey, host));
+    this.initialDialogId = INITIAL_DIALOG;
+  }
 
-    }
-
-    /**
-     * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
+  /**
+     * The run method handles the incoming activity (in the form of a TurnContext)
+     * and passes it through the dialog system.
      * If no dialog is active, it will start the default dialog.
-     * @param {*} turnContext
+     * @param {*} context
      * @param {*} accessor
      */
-    public async run(context: TurnContext, accessor: StatePropertyAccessor<DialogState>) {
-        const dialogSet = new DialogSet(accessor);
-        dialogSet.add(this);
-
-        const dialogContext = await dialogSet.createContext(context);
-        const results = await dialogContext.continueDialog();
-        if (results.status === DialogTurnStatus.empty) {
-            await dialogContext.beginDialog(this.id);
-        }
+  public async run(context: TurnContext, accessor: StatePropertyAccessor<DialogState>) {
+    const dialogSet = new DialogSet(accessor);
+    dialogSet.add(this );
+    const dialogContext = await dialogSet.createContext(context);
+    const results = await dialogContext.continueDialog();
+    if (results.status === DialogTurnStatus.empty) {
+      await dialogContext.beginDialog(this.id);
     }
+  }
 
-    // This is the first step of the WaterfallDialog.
-    // It kicks off the dialog with the QnA Maker with provided options.
-    private async startInitialDialog(step: { beginDialog: (arg0: string) => any; }) {
-
-        return await step.beginDialog(QNAMAKER_BASE_DIALOG);
-    }
+  // This is the first step of the WaterfallDialog.
+  // It kicks off the dialog with the QnA Maker with provided options.
+  private async startInitialDialog(step: { beginDialog: (arg0: string) => any; }) {
+    return await step.beginDialog(QNAMAKER_BASE_DIALOG);
+  }
 }
 
 module.exports.RootDialog = RootDialog;
